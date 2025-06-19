723f72fedfb6181051ce7b874e1688b73b44f740
jibrilMaygag <jibrilmeygag@gmail.com>
2025-06-19 17:08:34 +0300

    add clear terminal

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import random
+import os
+#for clearing the terminal
+def clear_console():
+    # Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # macOS/Linux
+    else:
+        os.system('clear')
+
 from hungman_words import word_list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 end_of_game = False
 lives = 6
-from hungman_art import logo
+from hungman_art import logo,stages
 print(logo)
 ##Testing code
 #print(f'Pssst, the solution is {chosen_word}.')
@@ -15,6 +25,7 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    clear_console()
 
     if guess in display:
         print(f"you've already guessed {guess}")
@@ -38,6 +49,4 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
-
-    from hungman_art import stages
     print(stages[lives])
